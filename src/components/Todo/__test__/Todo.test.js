d472bed3495013799d67ebb24d0350f86521b050
gergelykisshun <gergely.kiss.hun@gmail.com>
2022-06-04 13:24:52 +0200

    integration test setup in todo

---
+++ src/components/Todo/__test__/Todo.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Todo from '../Todo';
+
+describe("Todo integration tests", () => {
+  it('dont know yet', () => {
+    render(<Todo />);
+  })
+});
